1b1e14387122d673f8e7debe21bc13d0b7708057
Emma Åstrand <aastrand.emma@gmail.com>
2024-05-22 11:03:06 +0200

    feat: Add function for part 2

--- src/main/java/com/github/aastrandemma/App.java
+++ src/main/java/com/github/aastrandemma/App.java
@@ -2,11 +2,14 @@ package com.github.aastrandemma;
 
 import com.github.aastrandemma.config.ComponentScanConfig;
 import com.github.aastrandemma.data_access.StudentDao;
+import com.github.aastrandemma.util.UserInputService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ComponentScanConfig.class);
         StudentDao studentDao = context.getBean(StudentDao.class);
+        UserInputService userInputService = context.getBean(UserInputService.class);
     }
 }
---
+++ src/main/java/com/github/aastrandemma/util/impl/ScannerInputService.java
@@ -0,0 +1,29 @@
+package com.github.aastrandemma.util.impl;
+
+import com.github.aastrandemma.util.UserInputService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component
+public class ScannerInputService implements UserInputService {
+    private Scanner scanner;
+
+    @Autowired
+    public ScannerInputService(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    @Override
+    public String getString() {
+        if (scanner == null) throw new IllegalArgumentException("Input cannot be null");
+        return scanner.nextLine();
+    }
+
+    @Override
+    public int getInt() {
+        if (scanner == null) throw new IllegalArgumentException("Input cannot be null");
+        return scanner.nextInt();
+    }
+}
